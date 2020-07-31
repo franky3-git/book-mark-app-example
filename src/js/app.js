41217d392613fe79d091c835c59eb4bf1050a4b7
franky3-git <frankndounga@yahoo.fr>
2020-07-31 12:35:04 +0100

    add btn submit and btn remove functionnality

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,37 @@
 //"use strict"
 const log = console.log;
 
+//Dom elements
+const webName = document.getElementById('site-name');
+const webUrl = document.getElementById('site-url');
+const outputContainer = document.querySelector('.output-container');
+const form = document.querySelector('form');
+const template = document.querySelector('.template');
+
+//functions
+const addBookmark = function(e){
+	e.preventDefault();
+	if(webName.value && webUrl.value) {
+		let html = template.textContent;
+		html = html.replace('%name%', webName.value);
+		html = html.replace('%href%', webUrl.value);
+		outputContainer.insertAdjacentHTML('beforeend', html);
+		webName.value = '';
+		webUrl.value = '';
+	}
+}
+
+const removeBookmark = function(e){
+	if(e.target.classList.contains('btn-remove')) {
+		var item = e.target.parentElement;
+		item.remove();
+	}
+}
+
+//Events handlers
+form.addEventListener('submit', addBookmark)
+outputContainer.addEventListener('click', removeBookmark)
+
 
 
 
